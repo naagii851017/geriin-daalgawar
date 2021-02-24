1b12b7075aa7a439f418114cce4cdd604fe06f65
naagii851017 <77790650+naagii851017@users.noreply.github.com>
2021-02-24 15:58:04 +0800

    Update shagai.cpp

--- shagai.cpp
+++ shagai.cpp
@@ -1,32 +1,34 @@
-#include<iostream>
 #include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
 using namespace std;
 
 
 int main()
 {
-    
     int t;
     int h;
-	h=5;
+    int a1;
+    h=5;
     cin >> t;
-    for(int i = 0; i<t; i++)
-    {
-        int a,count = 0;
+    for(int i = 0; i<t && t<=100; i++){
+        int a, count = 0; 
+		bool nuhtsul = 0<a<=1000000;
         cin >> a;
         a--;
         double shagai;
-        int a1;
         shagai = a / h;
         a1=ceil(shagai);
-        
         while(a1!=0)
         {
-            if(a1%2)count++;
+            if(a1%2==0) count++;
             a1>>=1;
         }
-        if(count%2)cout << "Shine" << endl;
-        else cout << "Huuchin" << endl;
+        if(count%2==0)cout << "Huuchin" << endl;
+        else cout << "Shine" << endl;
     }
     return 0;
 }
